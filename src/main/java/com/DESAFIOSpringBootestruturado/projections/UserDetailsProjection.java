e407b3ca275887f9f984ee268eaa03bb0d556d96
Edvaldo Oliveira <ed.ferreiraoliveira@gmail.com>
2026-04-07 10:27:22 -0300

    Produto implementado

---
+++ src/main/java/com/DESAFIOSpringBootestruturado/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.DESAFIOSpringBootestruturado.projections;
+
+public interface UserDetailsProjection {
+
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
---
+++ src/main/java/com/DESAFIOSpringBootestruturado/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.DESAFIOSpringBootestruturado.repositories;
+
+
+import com.DESAFIOSpringBootestruturado.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
